5f1b5ea7f2f009ac1677eb31641c2a58e822c446
manhdt711 <dtm.it2002@gmail.com>
2024-07-01 22:08:26 +0700

    update Img

--- src/main/java/fpt/CapstoneSU24/service/CloudinaryService.java
+++ src/main/java/fpt/CapstoneSU24/service/CloudinaryService.java
@@ -109,4 +109,19 @@ public class CloudinaryService {
         );
     }
 
+    public void deleteImage(String publicId) {
+        try {
+            Map<String, Object> result = cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+            System.out.println("Image deleted: " + result.get("result"));
+        } catch (IOException e) {
+            throw new RuntimeException("Error deleting image from Cloudinary: ", e);
+        }
+    }
+
+    public void updateImage(String publicId, @org.jetbrains.annotations.NotNull MultipartFile updateFile) throws IOException {
+            deleteImage(publicId);
+            uploadImage(updateFile, publicId);
+    }
+
+
 }
